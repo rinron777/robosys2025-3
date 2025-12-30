b89e443012eae6b45f0db9093a387ded1d687885
rinron <github4273senyou564@gmail.com>
2025-12-30 14:06:37 +0900

    update

--- ros2_ws/test.bash
+++ ros2_ws/test.bash
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 source /opt/ros/humble/setup.bash
-
 cd "$(dirname "$0")/../.."
 
 colcon build --symlink-install
 source install/setup.bash
 
-timeout 5 ros2 run robosysros2_pkg cpu_usage_listener > /tmp/listener.log &
+timeout 5 ros2 run robosysros2_pkg cpu_usage_listener > /tmp/listener.log 2>&1 &
 sleep 2
 
-timeout 5 ros2 run robosysros2_pkg cpu_usage_publisher > /tmp/publisher.log &
+timeout 5 ros2 run robosysros2_pkg cpu_usage_publisher > /tmp/publisher.log 2>&1 &
 sleep 3
 
-grep "CPU Usage" /tmp/listener.log || exit 1
+grep "CPU Usage" /tmp/listener.log
 
